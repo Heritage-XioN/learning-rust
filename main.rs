0fb37a95b267f8435525d3fa0e9eb7830f59fd9e
HERITAGE-XION <iheritage934@gmail.com>
2026-03-19 12:54:35 +0100

    modified

--- main.rs
+++ main.rs
@@ -1,17 +1,6 @@
-// the compiler can be given specific commands eg:
-// #[allow(unused_variables)]
-
-
-
-// find out what this -> :: means
-// for getting values within a range use
-// ".." eg: 3..10 or 3..=10 
-// the "=" means that the last value is included in the range
-
-// main entry point
 fn main() {
     // make sure all statements end with ";"
-    // also rust is a bueaty in the sense that the compiler 
+    // also rust is a bueaty in the sense that the compiler
     // will almost always tell you what the exact issue is
     // and how to fix/resolve it
 
@@ -23,7 +12,7 @@ fn main() {
 }
 
 // function declaration just like the main
-fn learning_variables(){
+fn learning_variables() {
     println!("learing varibles starts here!!!!");
     // declare a variable
     let a: i32 = 23;
@@ -40,15 +29,15 @@ fn learning_variables(){
     let mut c: i32 = 5;
     c += 5;
 
-    // shadowing concept: this entails that a declared variable 
-    // can be re-declared and used somewhere else in the code 
+    // shadowing concept: this entails that a declared variable
+    // can be re-declared and used somewhere else in the code
     // without causing any issues. but i generally dont support using that
     // so if you want to learn about it then hit up the official docs
 
-    // variables can also be declared using destructuring 
+    // variables can also be declared using destructuring
     // in this case we destructured a tuple
     // remember adding "mut" makes it mutable
-    let ( mut d, e) = (2, 4);
+    let (mut d, e) = (2, 4);
     d += 8;
 
     // varibales can be created using destructuring assignments
@@ -56,7 +45,7 @@ fn learning_variables(){
     // using this ".." in the destructuring assignment we can tell the compiler to just give us the
     // value at the coresponding position as with our declared variable in this case f or g
     // also note the data structure when destructuring
-    (f, ..) = (25, 75); // tuple 
+    (f, ..) = (25, 75); // tuple
     [.., g] = [65, 21]; // array
 
     // the value that is assigned to a variable can be type annoted
@@ -64,8 +53,8 @@ fn learning_variables(){
     // and is only used for readability sake
     let h: u8 = 8_u8; // in this case 8 is the value u8 is the dataType
 
-    // a variable can only be assigned a value with matching dataType 
-    // if the value you wish to assign it is not of the same dataType 
+    // a variable can only be assigned a value with matching dataType
+    // if the value you wish to assign it is not of the same dataType
     // you can use "as" followed by the variables dataType the compiler
     // will convert the value to the specified data type if possible
     let i: u16 = 4_u8 as u16;
